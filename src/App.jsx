a30b64254b2e2369544775027415f3f8a93897ae
Bunyameen <bunyameen@gmail.com>
2023-01-16 03:19:40 +0100

    phoheBook: data-fetching performed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 import { ContactsList } from './components/ContactsList'
 import { SearchInput } from './components/SearchInput'
@@ -7,18 +7,25 @@ import { Form } from './components/Form'
 import { ContactsCard } from './components/ContactsCard'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   console.log(persons)
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [query, setQuery] = useState("")
   const [showAll, setShowAll] = useState(true)
+
+useEffect(() => {
+  axios.get("http://127.0.0.1:5174/persons")
+  .then((response) =>{
+    setPersons(response.data)
+  })
+
+}, [])
+
+
+
+
 
 
   const handleChange = (e) =>{
